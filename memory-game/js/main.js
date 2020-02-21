88dab587ea5851c78596a24d66582348c4bd6ff6
Arjun Rawal <arjunrawal@Arjuns-MacBook-Air.local>
2020-02-20 22:03:29 -0500

    created game logic

---
+++ memory-game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+let cardOne = "queen";
+let cardTwo = "king";
+let cardThree = "queen";
+let cardFour = "king";
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardFour);
